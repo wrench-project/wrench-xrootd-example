7a1f7d2fffa7eda3aa3fbe49c75d9172e5c3207a
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2022-05-30 00:07:12 -1000

    initial comit

---
+++ src/Controller.cpp
@@ -0,0 +1,106 @@
+
+/**
+ * Copyright (c) 2017-2021. The WRENCH Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+/**
+ ** An execution controller to execute a workflow
+ **/
+
+#define GFLOP (1000.0 * 1000.0 * 1000.0)
+#define MBYTE (1000.0 * 1000.0)
+#define GBYTE (1000.0 * 1000.0 * 1000.0)
+
+#include <iostream>
+#include <wrench/services/storage/xrootd/Node.h>
+#include "Controller.h"
+
+WRENCH_LOG_CATEGORY(controller, "Log category for Controller");
+
+namespace wrench {
+
+    /**
+     * @brief Constructor
+     *
+     * @param bare_metal_compute_service: a set of compute services available to run actions
+     * @param storage_services: a set of storage services available to store files
+     * @param hostname: the name of the host on which to start the WMS
+     */
+    Controller::Controller(std::shared_ptr<BareMetalComputeService> bare_metal_compute_service,
+                           const std::shared_ptr<XRootD::Node> &root,
+                           const std::string &hostname) : ExecutionController(hostname, "controller"),
+                                                          bare_metal_compute_service(bare_metal_compute_service), root(root) {}
+
+    /**
+     * @brief main method of the Controller
+     *
+     * @return 0 on completion
+     *
+     * @throw std::runtime_error
+     */
+    int Controller::main() {
+
+        /* Set the logging output to GREEN */
+        TerminalOutput::setThisProcessLoggingColor(TerminalOutput::COLOR_GREEN);
+        WRENCH_INFO("Controller starting");
+
+        /* Create a files */
+        std::vector<std::shared_ptr<DataFile>> files ={ 
+			wrench::Simulation::addFile("file1", 1 * GBYTE),
+			wrench::Simulation::addFile("file2", 1 * GBYTE),
+			wrench::Simulation::addFile("file3", 1 * GBYTE),
+			wrench::Simulation::addFile("file4", 1 * GBYTE),
+		};
+        
+        wrench::Simulation::createFile(files[0], root->getChild(0));
+		wrench::Simulation::createFile(files[1], root->getChild(1));
+		wrench::Simulation::createFile(files[2], root->getChild(2)->getChild(0));
+		wrench::Simulation::createFile(files[3], root->getChild(2)->getChild(1));
+
+        /* Create a job manager so that we can create/submit jobs */
+        auto job_manager = this->createJobManager();
+
+        WRENCH_INFO("Creating a compound job with an assortment of file reads");
+        auto job1 = job_manager->createCompoundJob("job1");
+        auto fileread1 = job1->addFileReadAction("fileread", files[0], root);
+		auto fileread2 = job1->addFileReadAction("fileread", files[1], root);
+		auto fileread3 = job1->addFileReadAction("fileread", files[2], root);
+		auto fileread4 = job1->addFileReadAction("fileread", files[3], root);
+
+        auto compute = job1->addComputeAction("compute", 100 * GFLOP, 50 * MBYTE, 1, 3, wrench::ParallelModel::AMDAHL(0.8));
+        job1->addActionDependency(fileread1, compute);
+        job1->addActionDependency(fileread2, fileread3);
+        job1->addActionDependency(fileread3, fileread4);
+		job1->addActionDependency(fileread4, compute);
+        
+        
+        job_manager->submitJob(job1, this->bare_metal_compute_service);
+        
+
+        WRENCH_INFO("Execution complete!");
+
+        std::vector<std::shared_ptr<wrench::Action>> actions = {fileread1, fileread2, fileread3, fileread4,compute};
+        for (auto const &a: actions) {
+            printf("Action %s: %.2fs - %.2fs\n", a->getName().c_str(), a->getStartDate(), a->getEndDate());
+        }
+
+        return 0;
+    }
+
+    /**
+     * @brief Process a compound job completion event
+     *
+     * @param event: the event
+     */
+    void Controller::processEventCompoundJobCompletion(std::shared_ptr<CompoundJobCompletedEvent> event) {
+        /* Retrieve the job that this event is for */
+        auto job = event->job;
+        /* Print info about all actions in the job */
+        WRENCH_INFO("Notified that compound job %s has completed:", job->getName().c_str());
+    }
+}// namespace wrench
---
+++ src/Simulator.cpp
@@ -0,0 +1,92 @@
+
+/**
+ * Copyright (c) 2017-2021. The WRENCH Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+/**
+ ** This is the main function for a WRENCH simulator. The simulator takes
+ ** a input an XML platform description file. It generates a workflow with
+ ** a simple diamond structure, instantiates a few services on the platform, and
+ ** starts an execution controller to execute the workflow using these services
+ ** using a simple greedy algorithm.
+ **/
+
+#include <iostream>
+#include <wrench-dev.h>
+#include <wrench/services/storage/xrootd/XRootD.h>
+
+#include <wrench/services/storage/xrootd/Node.h>
+#include "Controller.h"
+
+/**
+ * @brief The Simulator's main function
+ *
+ * @param argc: argument count
+ * @param argv: argument array
+ * @return 0 on success, non-zero otherwise
+ */
+int main(int argc, char **argv) {
+
+    /* Create a WRENCH simulation object */
+    auto simulation = wrench::Simulation::createSimulation();
+
+    /* Initialize the simulation */
+    simulation->init(&argc, argv);
+
+    /* Parsing of the command-line arguments */
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <xml platform file> [--log=controller.threshold=info | --wrench-full-log]" << std::endl;
+        exit(1);
+    }
+
+    /* Instantiating the simulated platform */
+    simulation->instantiatePlatform(argv[1]);
+
+    /* Instantiate a storage service on the platform */
+    //auto storage_service = simulation->add(new wrench::SimpleStorageService("StorageHost", {"/"}, {}, {}));
+
+    /* Instantiate a bare-metal compute service on the platform */
+    auto baremetal_service = simulation->add(new wrench::BareMetalComputeService("Fafard", {"Fafard"}, "", {}, {}));
+	simulation->add(baremetal_service.get());
+
+    /* Instantiate an execution controller */
+    
+	/*Construct XRootD tree that looks like
+	             Ginette
+	          /     |     \ 	
+	 Timberlay Jacquellin  Jupiter
+	                      /       \ 	
+	                  Bourassa   Boivin
+	*/
+	wrench::XRootD::XRootD xrootdManager;
+	std::shared_ptr<wrench::XRootD::Node> root=xrootdManager.createSupervisor("Ginette");
+	std::shared_ptr<wrench::XRootD::Node> activeNode=xrootdManager.createStorageServer("Tremblay",{},{});
+	root->addChild(activeNode);
+	simulation->add(activeNode.get());
+	activeNode=xrootdManager.createStorageServer("Jacquelin",{},{});
+	root->addChild(activeNode);
+	simulation->add(activeNode.get());
+	activeNode=xrootdManager.createSupervisor("Jupiter");
+	root->addChild(activeNode);
+	simulation->add(activeNode.get());
+	
+	std::shared_ptr<wrench::XRootD::Node> leaf=xrootdManager.createSupervisor("Bourassa");
+	activeNode->addChild(leaf);
+	simulation->add(leaf.get());
+	leaf=xrootdManager.createSupervisor("Boivin");
+	activeNode->addChild(leaf);
+	simulation->add(leaf.get());
+	
+	simulation->add(root.get());
+    
+    /* Launch the simulation */
+	auto controller = simulation->add(        new wrench::Controller(baremetal_service, root, "Fafard"));
+    simulation->launch();
+
+    return 0;
+}
